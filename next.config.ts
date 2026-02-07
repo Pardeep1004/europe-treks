b9364ec9ef8c5f503fb8e8043d9e99f667b991ec
Pardeep1004 <pardeepnirwan4@gmail.com>
2026-02-07 14:32:53 +0100

    Fix: Remove local

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: 'c:/Users/Pardeep Nirwan/europe-treks',
-  },
-  outputFileTracingRoot: 'c:/Users/Pardeep Nirwan/europe-treks',
-} as any;
+};
 
 export default nextConfig;
